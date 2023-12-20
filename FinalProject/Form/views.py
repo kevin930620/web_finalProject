d2c3141f9cf0e0a245b0e8ad82ccedcfab8cacbf
kevin930620 <kevin0974004568@gmail.com>
2023-12-20 17:43:12 +0800

    login fail

--- FinalProject/Form/views.py
+++ FinalProject/Form/views.py
@@ -9,7 +9,15 @@ from Form.form import LoginForm
 def login(request):
     template = loader.get_template('login.html')
     messages = ''
-    if request.method == "POST":
+    if request.method == 'GET':
+        post_form = LoginForm()
+        context={
+            'user': request.user,
+            'post_form':post_form,
+        }
+        return HttpResponse(template.render(context,request))
+
+    elif request.method == "POST":
         post_form = LoginForm(request.POST)
         if post_form.is_valid():
             username = post_form.changed_data['username']
@@ -19,20 +27,16 @@ def login(request):
                 auth.login(request,user)
                 messages = f'{username} Login'
                 main_html = loader.get_template('main.html')
-                context = {'user': request.user}
+                context = {'user': request.user,
+                           'messages':'login ok'}
                 return HttpResponse(main_html.render(context,request))
             else:
                 messages = 'Login failed'
-            print(messages)
+        else:
+            print('Login ERROR')
     else:
-        post_form = LoginForm()
-    context = {
-        'user' : request.user,
-        'post_form': post_form,
-        'message' : messages,
-    }
-    return HttpResponse(template.render(context,request))
-
+        print('ERROR')
+    
 def logout(request):
     auth.logout(request)
     main_html = loader.get_template('main.html')
